42d1e389e9ccf1bea5b1724e6cbc8a8b1adc1be2
irkyo <2044161669@qq.com>
2020-01-28 09:40:24 +0800

    添加项目文件。

---
+++ PktSender/resource.h
@@ -0,0 +1,29 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 PktSender.rc 使用
+//
+#define IDD_PKTSENDER_DIALOG            102
+#define IDR_MAINFRAME                   128
+#define IDC_BT_OPEN                     1000
+#define IDC_EDIT_PATH                   1001
+#define IDC_BT_SETTING                  1003
+#define IDC_BT_SEND                     1004
+#define IDC_CHECK_SIGPKT                1005
+#define IDC_EDIT_PKTNUM                 1006
+#define IDC_CHECK_SETPKTNUM             1008
+#define IDC_BT_RESEND                   1009
+#define IDC_BT_STOP                     1010
+#define IDC_COMBO_SENDMODE              1012
+#define IDC_CHECK1                      1013
+#define IDC_CHECK_ONTOP                 1013
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1014
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
